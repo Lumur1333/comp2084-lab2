26c378f326170484ffe4ac09b2cd39355f1622d6
Lumur1333 <200304037@student.georgianc.on.ca>
2020-06-01 15:21:30 -0400

    Repository Changes

---
+++ Controllers/CategoriesControllercs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Food()
+        {
+            return View();
+        }
+
+        public IActionResult Tech()
+        {
+            return View();
+        }
+
+        public IActionResult Sports()
+        {
+            return View();
+        }
+    }
+}
